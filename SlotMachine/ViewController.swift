583ce924c8ccf74d8b1e1d9c071992f2bc9d7496
Jacy Wang <jacywj@gmail.com>
2015-02-11 20:00:06 -0500

    setupThirdContainer

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -17,6 +17,16 @@ class ViewController: UIViewController {
     
     var titleLabel: UILabel!
     
+    // Information labels
+    
+    var creditsLabel: UILabel!
+    var betLabel: UILabel!
+    var winnerLabel: UILabel!
+    var creditsTitleLabel: UILabel!
+    var betTitleLabel: UILabel!
+    var winnerTitleLabel: UILabel!
+    
+    
     let kMarginForView: CGFloat = 10.0
     let kSixth: CGFloat = 1.0/6.0
     let kThird: CGFloat = 1.0/3.0
@@ -32,6 +42,7 @@ class ViewController: UIViewController {
         setupContainerViews()
         setupFirstContainer(self.firstContainer)
         setupSecondContainer(self.secondContainer)
+        setupThirdContaine(self.thirdContainer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -76,6 +87,66 @@ class ViewController: UIViewController {
                 containerView.addSubview(slotImageView)
             }
         }
+    }
+    
+    func setupThirdContaine(containerView: UIView) {
+        self.creditsLabel = UILabel()
+        self.creditsLabel.text = "000000"
+        self.creditsLabel.textColor = UIColor.redColor()
+        self.creditsLabel.font = UIFont(name: "Menlo-bold", size: 16)
+        self.creditsLabel.sizeToFit()
+        self.creditsLabel.center = CGPoint(x: containerView.frame.width * kSixth, y: containerView.frame.height * kThird)
+        self.creditsLabel.backgroundColor = UIColor.darkGrayColor()
+        self.creditsLabel.textAlignment = NSTextAlignment.Center
+        containerView.addSubview(self.creditsLabel)
+        
+        self.betLabel = UILabel()
+        self.betLabel.text = "0000"
+        self.betLabel.textColor = UIColor.redColor()
+        self.betLabel.font = UIFont(name: "Menlo-bold", size: 16)
+        self.betLabel.sizeToFit()
+        self.betLabel.center = CGPoint(x: containerView.frame.width * kSixth * 3, y: containerView.frame.height * kThird)
+        self.betLabel.textAlignment = NSTextAlignment.Center
+        self.betLabel.backgroundColor = UIColor.darkGrayColor()
+        containerView.addSubview(self.betLabel)
+        
+        self.winnerLabel = UILabel()
+        self.winnerLabel.text = "000000"
+        self.winnerLabel.textColor = UIColor.redColor()
+        self.winnerLabel.font = UIFont(name: "Menlo-bold", size: 16)
+        self.winnerLabel.sizeToFit()
+        self.winnerLabel.center = CGPoint(x: containerView.frame.width * kSixth * 5, y: containerView.frame.height * kThird)
+        self.winnerLabel.textAlignment = NSTextAlignment.Center
+        self.winnerLabel.backgroundColor = UIColor.darkGrayColor()
+        containerView.addSubview(self.winnerLabel)
+        
+        self.creditsTitleLabel = UILabel()
+        self.creditsTitleLabel.text = "Credits"
+        self.creditsTitleLabel.textColor = UIColor.blackColor()
+        self.creditsTitleLabel.font = UIFont(name: "AmericanTypewriter", size: 14)
+        self.creditsTitleLabel.sizeToFit()
+        self.creditsTitleLabel.center = CGPoint(x: containerView.frame.width * kSixth, y: containerView.frame.height * kThird * 2)
+        self.creditsTitleLabel.textAlignment = NSTextAlignment.Center
+        containerView.addSubview(self.creditsTitleLabel)
+        
+        self.betTitleLabel = UILabel()
+        self.betTitleLabel.text = "Bet"
+        self.betTitleLabel.textColor = UIColor.blackColor()
+        self.betTitleLabel.font = UIFont(name: "AmericanTypewriter", size: 14)
+        self.betTitleLabel.sizeToFit()
+        self.betTitleLabel.center = CGPoint(x: containerView.frame.width * kSixth * 3, y: containerView.frame.height * kThird * 2)
+        self.betTitleLabel.textAlignment = NSTextAlignment.Center
+        containerView.addSubview(self.betTitleLabel)
+        
+        self.winnerTitleLabel = UILabel()
+        self.winnerTitleLabel.text = "Winner Paid"
+        self.winnerTitleLabel.textColor = UIColor.blackColor()
+        self.winnerTitleLabel.font = UIFont(name: "AmericanTypewriter", size: 14)
+        self.winnerTitleLabel.sizeToFit()
+        self.winnerTitleLabel.center = CGPoint(x: containerView.frame.width * kSixth * 5, y: containerView.frame.height * kThird * 2)
+        self.winnerTitleLabel.textAlignment = NSTextAlignment.Center
+        containerView.addSubview(self.winnerTitleLabel)
+        
     }
 
 
